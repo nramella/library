5a30b670db53e2862cbbbe53935e92cc254dd151
Nick Ramella <nickramella15@yahoo.com>
2019-04-18 16:49:46 -0400

    Added delete function

--- scripts/script.js
+++ scripts/script.js
@@ -2,14 +2,7 @@
 const newBookBtn = document.getElementById('newBookBtn');
 const form = document.getElementById('newBookForm')
 const submitBtn = document.getElementById('submit')
-const list = document.getElementById('libraryList');
-
-// Listeners //
-window.onload = function() {
-    form.style.display = "none"; // Hides the new book form when page loads
-};
-newBookBtn.addEventListener("click", checkDisplayForm);
-submitBtn.addEventListener("click", getNewBook);
+const table = document.getElementById('library-table');
 
 let myLibrary = []; // Library array to hold current books
 
@@ -21,21 +14,43 @@ function Book(title, author) {
     }
 }
 
+// Adds book to the library array, then renders the book
 function addBookToLibrary(title, author) {
     const newBook = new Book(title, author);
     myLibrary.push(newBook);
+    render(newBook);
+
 }
 
-function render() {
-    for(var i=0; i<myLibrary.length; i++) {
-        const row = document.createElement('div');
-        row.setAttribute("class", "row");
-        const currentBook = myLibrary[i];
-        row.innerHTML = currentBook.title+ " by: "+currentBook.author;
-        list.append(row);
-    }
+// Renders the library onto the screen in a table
+function render(book) {
+    const row = document.createElement('tr');
+    const titleTD = document.createElement('td');
+    const authorTD = document.createElement('td');
+    const deleteTD = document.createElement('td');
+    const deleteBtn = document.createElement('input');
+    deleteBtn.type = "button";
+    deleteBtn.value = "Delete";
+    deleteBtn.setAttribute("id", myLibrary.length-1);
+    // deleteRender.setAttribute("onClick", "deleteBook(this.id);");
+
+    titleTD.innerHTML = book.title;
+    authorTD.innerHTML = book.author;
+
+    deleteBtn.addEventListener('click', function(){
+        row.remove();
+        myLibrary.splice(this.id, 1);
+        console.log(myLibrary.length)
+    });
+
+    row.appendChild(titleTD);
+    row.appendChild(authorTD);
+    deleteTD.appendChild(deleteBtn);
+    row.appendChild(deleteTD);
+    table.appendChild(row);
 }
 
+// Controls when the form is displayed 
 function checkDisplayForm() {
     if (form.style.display === "none"){
         form.style.display = "block";
@@ -45,22 +60,23 @@ function checkDisplayForm() {
     }
 }
 
+// Gets the information from the new book form
 function getNewBook() {
     const inputTitle = document.getElementById('inputTitle');
     const inputAuthor = document.getElementById('inputAuthor');
     addBookToLibrary(inputTitle.value, inputAuthor.value);
     inputTitle.value = '';
     inputAuthor.value = '';
-    refresh();
+    checkDisplayForm();
 }
 
-function refresh() {
-    while (list.firstChild) {
-        list.removeChild(list.firstChild);
-    }
-    checkDisplayForm();
-    render();
-}
 
 addBookToLibrary('Memory Man', 'David Baldacci'); // Sample book (Testing Only - will be removed)
-render();
+
+
+// Listeners //
+window.onload = function() {
+    form.style.display = "none"; // Hides the new book form when page loads
+};
+newBookBtn.addEventListener("click", checkDisplayForm);
+submitBtn.addEventListener("click", getNewBook);
